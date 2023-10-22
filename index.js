e1aa921caa0487cb47899e7eb54cd368cdd789d3
Emre Memiş <dev.emrememis@gmail.com>
2023-10-22 18:34:19 +0300

    change port

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const PORT = 8001;
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("App Start");
 });
