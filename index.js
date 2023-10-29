3a189eea1201f7f2329b3dab18a60dd380cab5b6
Emre Memiş <dev.emrememis@gmail.com>
2023-10-29 15:10:39 +0300

    basic refactor

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
-require("dotenv").config();
+const ENV = require("dotenv");
+ENV.config();
 const app = express();
 const PORT = process.env.PORT;
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
