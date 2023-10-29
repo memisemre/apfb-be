aeb8b87c68c437e53a9a218dc8380ff69380e2c0
Emre Memiş <dev.emrememis@gmail.com>
2023-10-29 15:48:28 +0300

    add mongodb

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-const express = require("express");
-const ENV = require("dotenv");
-ENV.config();
+import express from "express";
+import { config } from "dotenv";
+config();
 const app = express();
 const PORT = process.env.PORT;
+
+import { MongoClient, ServerApiVersion } from "mongodb";
+const uri = process.env.MONGODB_URL;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
 app.listen(PORT, () => {
   console.log("App Start");
   console.log(`Example app listening on port ${PORT}`);
